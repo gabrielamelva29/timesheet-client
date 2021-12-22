cf3392b1ac0cd20fa891fad92a0aae6a1b4b3ddc
Donytriprasetyo <donyprasetyo84@gmail.com>
2021-12-22 10:32:29 +0700

    refine form employee, form log, form history in hr, adding alert delete

--- src/main/resources/static/datepicker/js/datepicker.js
+++ src/main/resources/static/datepicker/js/datepicker.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
     var date_input = $('input[name="date"]');
     var container = $('.bootstrap-iso form').length > 0 ? $('.bootstrap-iso form').parent() : "body";
     date_input.datepicker({
+//        language: 'id',
         format: 'dd MM yyyy',
 //                    container: container,
         todayHighlight: true,
---
+++ src/main/resources/static/sweetalert/delete.js
@@ -0,0 +1,23 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function deleteById() {
+    event.preventDefault();
+    var form = event.target.form;
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            form.submit();
+        }
+    });
+}
+
