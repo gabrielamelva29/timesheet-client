881ab8298c82440387d05a8c1cafbf92c16d2809
Donytriprasetyo <donyprasetyo84@gmail.com>
2022-02-01 19:10:39 +0700

    adding sending email in send, approve and reject timesheet

--- src/main/resources/static/js/jobhistory.js
+++ src/main/resources/static/js/jobhistory.js
@@ -7,11 +7,7 @@ $(document).ready(() => {
     if (urlParams.get('sent') == 'false') {
         failMessage("This Period Must be Full");
     }
-    if (urlParams.get('sent') == 'true') {
-        successMessage("This Period was Successfully Sent");
-    }
 });
-
 
 function failMessage(gagal) {
     Swal.fire({
@@ -31,6 +27,138 @@ function successMessage(berhasil) {
     }).then((result) => {
         if (result.isConfirmed) {
             window.location.href = "history";
+        }
+    });
+}
+
+function send(id) {
+    $("#sentForm").attr("class", "sent-" + id);
+    event.preventDefault();
+    const urlAct = "/sent/" + id;
+
+    $.ajax({
+        url: urlAct,
+        type: 'POST',
+        dataType: 'json',
+        data: $('.sent-' + id).serialize(),
+        beforeSend: (req) => {
+            addRequestHeader();
+        },
+        success: (result) => {
+            
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+}
+
+function confirmationSend(id){
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, sent it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            send(id);
+            successMessage("Send was Successfully")
+        }
+    });
+}
+
+function approved(id) {
+    $("#approveForm").attr("class", "approve-" + id);
+    event.preventDefault();
+    const urlAct = "/approved/" + id;
+
+    $.ajax({
+        url: urlAct,
+        type: 'POST',
+        dataType: 'json',
+        data: $('.approve-' + id).serialize(),
+        beforeSend: (req) => {
+            addRequestHeader();
+        },
+        success: (result) => {
+            
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+}
+
+function confirmationApproved(id){
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, approve it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            approved(id);
+            successMessageReject("Approved was Successfully")
+        }
+    });
+}
+
+function reject(id) {
+    $("#rejectForm").attr("class", "reject-" + id);
+    event.preventDefault();
+    const urlAct = "/rejected/" + id;
+
+    $.ajax({
+        url: urlAct,
+        type: 'POST',
+        dataType: 'json',
+        data: $('.reject-' + id).serialize(),
+        beforeSend: (req) => {
+            addRequestHeader();
+        },
+        success: (result) => {
+            
+        },
+        error: (err) => {
+            console.log(err);
+        }
+    });
+}
+
+
+function confirmationReject(id){
+    event.preventDefault();
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, reject it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            reject(id);
+            successMessageReject("Rejected was Successfully")
+        }
+    });
+}
+
+function successMessageReject(berhasil) {
+    Swal.fire({
+        icon: 'success',
+        title: berhasil
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = "hr";
         }
     });
 }
